cb58cce6ad3d0aed1f7cbfbea6ad7169dd347092
Veer1107 <104772820+Veer1107@users.noreply.github.com>
2022-05-02 18:48:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function loginVerification(){
+var username =document.getElementById("Username").value;
+var password=document.getElementById("Password").value;
+if(username=="veeraselvann@gmail.com" && password=="18ME1107"){
+
+  location.replace('/index2.html');
+}
+}
+function fly(){
+  alert("I was called");
+}
